ca52c8929a401a9c8b208dcfdbb1a38b76c8a311
alvin <302767720@qq.com>
2018-07-09 23:36:34 +0800

    增加根据用户名批量查询接口

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -3,14 +3,16 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import utils.MybatisUtil;
 
+import java.util.List;
+
 public class UserTest {
 
     SqlSession sqlSession ;
     @Test
     public void insertPerson(){
         sqlSession = MybatisUtil.getSqlSession();
-        int id = 10000;
-        String userName = "test";
+        int id = 12;
+        String userName = "test_12";
         int age = 18;
         String mobilePhone = "18000000000";
         Person person = new Person();
@@ -36,8 +38,28 @@ public class UserTest {
         try{
             Person person = sqlSession.selectOne("queryById",id);
 
-            sqlSession.commit();
+//            sqlSession.commit();
             System.out.println(person.getUserName());
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            MybatisUtil.closeSession(sqlSession);
+        }
+
+    }
+
+    @Test
+    public void queryByUserName(){
+        sqlSession = MybatisUtil.getSqlSession();
+        int id = 10000;
+        try{
+            List<Person> persons = sqlSession.selectList("queryByUserNames","test");
+
+//            sqlSession.commit();
+            for(int i=0;i<persons.size();i++){
+                System.out.println(persons.get(i).getUserName());
+            }
+
         }catch (Exception e){
             e.printStackTrace();
         }finally {
